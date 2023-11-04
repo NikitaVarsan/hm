355e1e65ce4866510fbaf468c4eb5819beb3b7ef
NikitaVarsan <NekitVars@icloud.com>
2023-11-04 23:43:11 +0100

    homework 5

---
+++ hm5.py
@@ -0,0 +1,18 @@
+first_num = int(input("Please enter first number: "))
+second_num = int(input("Please enter second number: "))
+action = input("Please enter action: ")
+
+if action == "+":
+    print("Your result is: ", first_num + second_num)
+elif action == "-":
+    print("Your result is ", first_num - second_num)
+elif action == "*":
+    print("Your result is ", first_num * second_num)
+elif action == "/":
+    if second_num == 0:
+        print("You can't divide by 0")
+    else:
+        print("Your result is ", first_num / second_num)
+
+
+
