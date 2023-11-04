09f717e2546d9c5b86771043758649c99ae3bba9
NikitaVarsan <NekitVars@icloud.com>
2023-11-04 20:18:13 +0100

    homework 1

--- hm2.py
+++ hm2.py
@@ -1,9 +1,3 @@
-number = int(input("Enter a 4 digit number: "))
-
-fourth_num = number % 10
-third_num = (number % 100) // 10
-second_num = (number % 1000) // 100
-first_num = number // 1000
-
-
-print(fourth_num , third_num, second_num, first_num)
+number = int(input("Enter a five digit number: "))
+reversed_number = int(str(number)[::-1])
+print(reversed_number)
